d1b059272ee13f9f91fdc485df563b07c054b5d7
Amit Arora <aroraai@amazon.com>
2023-05-05 12:56:19 +0000

    remove unused files, update cfn tempalte

--- blogs/rag/api/deps/httptools/parser/__init__.py
+++
@@ -1,4 +0,0 @@
-from .parser import *  # NoQA
-from .errors import *  # NoQA
-
-__all__ = parser.__all__ + errors.__all__  # NoQA
--- blogs/rag/api/deps/langchain/tools/ddg_search/__init__.py
+++
@@ -1,5 +0,0 @@
-"""DuckDuckGo Search API toolkit."""
-
-from langchain.tools.ddg_search.tool import DuckDuckGoSearchTool
-
-__all__ = ["DuckDuckGoSearchTool"]
